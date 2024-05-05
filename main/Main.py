1dfa29ea873a602d764b25d7eab731e21651df06
kicconsu <sicconsu009@gmail.com>
2024-05-05 15:25:19 -0500

    AASGHGH

--- main/Main.py
+++
@@ -1,9 +0,0 @@
-from core.parqueadero.pisos.Piso import Piso
-from main.core.parqueadero.Parqueadero import Parqueadero
-from main.core.parqueadero.vehiculos.Auto import Auto
-
-parqueadero = Parqueadero()
-
-parqueadero.addVehicle(0, "A2", Auto("MXN569", False), "ALGUNAHORAAHÍ")
-
-parqueadero.getFloor(0).getSlotByName("A2").print()
